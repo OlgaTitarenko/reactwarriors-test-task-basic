f6bbbf92f286c56989eefd283cd503727b68a671
olgaTitarenko <kizuneworld@gmail.com>
2019-10-25 22:08:50 +0300

    Updates

---
+++ precache-manifest.4531a007b8e7497378af3904817e524e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c6dfe92bae99e9206b10acbb9cab4b9",
+    "url": "/OlgaTitarenko/reactwarriors-test-task-basic/index.html"
+  },
+  {
+    "revision": "3f013212df2e70c71d57",
+    "url": "/OlgaTitarenko/reactwarriors-test-task-basic/static/css/main.d23d3239.chunk.css"
+  },
+  {
+    "revision": "b3a71faa42c5d3a6f2db",
+    "url": "/OlgaTitarenko/reactwarriors-test-task-basic/static/js/2.3427f577.chunk.js"
+  },
+  {
+    "revision": "3f013212df2e70c71d57",
+    "url": "/OlgaTitarenko/reactwarriors-test-task-basic/static/js/main.7053fdca.chunk.js"
+  },
+  {
+    "revision": "5fa18ecbebe147017292",
+    "url": "/OlgaTitarenko/reactwarriors-test-task-basic/static/js/runtime~main.f71d42e7.js"
+  }
+]);
